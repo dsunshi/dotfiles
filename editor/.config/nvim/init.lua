948ac6e702017aaa5e1b06b5ced08480326ec52c
David Sunshine <david@sunshines.org>
2021-06-13 21:38:59 +0200

    Minimal working example

---
+++ editor/.config/nvim/init.lua
@@ -0,0 +1,31 @@
+
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+    -- have Packer manage itself
+    use { 'wbthomason/packer.nvim', opt = true }
+
+
+end)
+
+
+-- Map leader to space
+vim.g.mapleader = ' '
+
+-- vim.o sets global options, e.g.: vim.o.hidden
+-- vim.bo sets only buffer-scoped options, e.g.: vim.bo.expandtab = true
+-- vim.wo sets only window-scoped options, e.g.: vim.wo.number = true
+
+-- This method of setting the scope is a temporary work-around until this PR is merged:
+-- https://github.com/neovim/neovim/pull/13479
+
+-- Help with setting up scops, stolen from: https://github.com/shaunsingh/vimrc-dotfiles/blob/main/.config/nvim/init.lua
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+
+opt('o', 'relativenumber', true)
