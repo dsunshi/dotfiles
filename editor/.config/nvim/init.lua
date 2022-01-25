6cdae21c29e03f9bec8e7fea18e8c567be09e7ea
David Sunshine <david@sunshines.org>
2022-01-25 11:36:25 -0500

    Added <leader>gs as a way to use telescope to display git status

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -143,6 +143,7 @@ map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_co
 map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", default_opts)
 map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", default_opts)
 map("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", default_opts)
+map('n', '<leader>gs', "<cmd>lua require'telescope.builtin'.git_status()<cr>", default_opts)
 
 vim.g.rustfmt_autosave = true   -- Format rust code when the buffer is saved
 
