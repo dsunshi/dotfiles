4a57a4a4dd4c61dca16912c2c1cfb86c117cc625
David Sunshine <david@sunshines.org>
2022-02-05 16:12:12 -0500

    Minor tweaks
    starship.rs and colorscript for some prompt improvements.
    remove conky from i3 config.

--- term/.config/fish/config.fish
+++ term/.config/fish/config.fish
@@ -38,3 +38,9 @@ set -g fish_pager_color_progress $comment
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
+
+colorscript -e 35
+
+# starship.rs
+starship init fish | source
+
