e90f4df716e423e900231abe954b7a2f5c0530c2
David Sunshine <david@sunshines.org>
2022-07-18 20:24:26 -0400

    Further switch to catpuccin color scheme

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -17,7 +17,11 @@ require('packer').startup(function(use)
 
     -- Appearance
     -- =========================================================================
-    use 'EdenEast/nightfox.nvim'         -- Color scheme
+   -- use 'EdenEast/nightfox.nvim'         -- Color scheme
+    use{
+        "catppuccin/nvim",
+        as = "catppuccin"
+    }
     use 'xiyaowong/nvim-transparent'     -- Transparency enable/disable
     use 'norcalli/nvim-colorizer.lua'    -- Display colors in Vim
 
@@ -99,45 +103,15 @@ end)
 -- =============================================================================
 -- Colorscheme {{{1
 -- =============================================================================
--- require('nightfox').load('duskfox')
-require('nightfox').setup({
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath("cache") .. "/duskfox",
-    compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = false,    -- Disable setting background
-    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = true,   -- Non focused panes set to alternative background
-    styles = {              -- Style to be applied to different syntax groups
-      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "NONE",
-      functions = "NONE",
-      keywords = "NONE",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "NONE",
-      types = "NONE",
-      variables = "NONE",
-    },
-    inverse = {             -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    modules = {             -- List of various plugins and additional options
-      -- ...
-    },
-  }
-})
 
 -- setup must be called before loading
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd("colorscheme nightfox")
+vim.g.catppuccin_flavour = "mocha"
+vim.cmd("colorscheme catppuccin")
 require('lualine').setup {
   options = {
-    theme = 'nightfox'
+    theme = 'catppuccin'
   }
 }
 
