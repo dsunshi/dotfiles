7d0d41ca8c130a1addbf6248a96e35d1e31e8ad0
David Sunshine <david@sunshines.org>
2022-10-25 20:15:56 -0400

    Updated emacs config and added org document

--- editor/.emacs.d/init.el
+++ editor/.emacs.d/init.el
@@ -6,32 +6,13 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; Disable the startup screen
-(setq inhibit-startup-message t)
-
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
 ;; Package manager
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-;; Try will allow packages to be used without actually installing them
-(use-package try
-  :ensure t)
 
-;; Enable help pop-up window for incomplete key-sequences
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-;; Themes
-(use-package zenburn-theme
-  :ensure t)
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-init.org"))
 
-(load-theme 'zenburn t)
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+;; Auto-generated
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -39,10 +20,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" default))
- '(package-selected-packages '(which-key zenburn-theme use-package evil)))
+ '(package-selected-packages
+   '(yasnippet-snippets yasnippet auto-complete undo-tree ace-window org-bullets which-key zenburn-theme use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
